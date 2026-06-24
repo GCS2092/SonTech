19d4893ea06f3e6d9c2b178c4b0072ee39227522
GCS2092 <slovengama05@gmail.com>
2026-06-24 22:51:52 +0000

    fix: pass SONTECH storeId to products API

--- src/api/products.api.js
+++ src/api/products.api.js
@@ -1,7 +1,8 @@
 import api from './axios';
+import { STORE_ID } from '../utils/constants';
 
 export const productsApi = {
-  getAll: (params) => api.get('/api/products', { params }),
+  getAll: (params) => api.get('/api/products', { params: { ...params, storeId: STORE_ID } }),
   getBySlug: (slug) => api.get(`/api/products/${slug}`),
   create: (data) => api.post('/api/products', data),
   update: (id, data) => api.put(`/api/products/${id}`, data),
